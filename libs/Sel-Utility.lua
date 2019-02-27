16906165343aeab6956f2d184dfa498ba504bb9e
KalesAndRancor <some___guy@hotmail.com>
2019-02-27 04:23:44 -0600

    Refined standardize set causing issues.

--- libs/Sel-Utility.lua
+++ libs/Sel-Utility.lua
@@ -2304,16 +2304,16 @@ function get_effective_player_tp(spell, WSset)
 end
 
 function standardize_set(set)
-	if not set.ear1 then set.ear1 = set.left_ear or '' end
-	if not set.ear2 then set.ear2 = set.right_ear or '' end
-	if not set.ring1 then set.ring1 = set.left_ring or '' end
-	if not set.ring2 then set.ring2 = set.right_right or '' end
+	local standardized_set = table.copy(set)
+	standardized_set.ear1 = standardized_set.ear1 or standardized_set.left_ear or standardized_set.lear or ''
+	standardized_set.ear2 = standardized_set.ear2 or standardized_set.right_ear or standardized_set.rear or ''
+	standardized_set.ring1 = standardized_set.ring1 or standardized_set.left_ring or standardized_set.rring or ''
+	standardized_set.ring2 = standardized_set.ring2 or standardized_set.right_right or standardized_set.lring or ''
 	
-	for slot in pairs(set) do
+	for slot in pairs(standardized_set) do
 		if type(slot) == 'table' and slot.name then slot = slot.name end
 	end
-	
-	return set
+	return standardized_set
 end
 
 function get_fencer_tp_bonus(WSset)
