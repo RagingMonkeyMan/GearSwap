d58d909c435a65a83783846a66d673d7cb4a6ae6
KalesAndRancor <some___guy@hotmail.com>
2019-08-10 09:19:58 -0500

    Fix labelling issue, add pet engaged check to pet enmity/ws gear.

--- data/PUP.lua
+++ data/PUP.lua
@@ -33,7 +33,7 @@ function job_setup()
 	state.AutoRepairMode = M(true, 'Auto Repair Mode')
 	state.AutoDeployMode = M(true, 'Auto Deploy Mode')
 	state.PetWSGear		 = M(true, 'Pet WS Gear')
-	state.PetEnmityGear	 = M(true, 'Pet WS Gear')
+	state.PetEnmityGear	 = M(true, 'Pet Enmity Gear')
 
     autows = "Victory Smite"
 	autofood = 'Akamochi'
--- data/Selindrile/Selindrile_Pup_Gear.lua
+++ data/Selindrile/Selindrile_Pup_Gear.lua
@@ -183,17 +183,21 @@ function init_gear_sets()
 	head=gear.herculean_fc_head,neck="Voltsurge Torque",ear1="Enchntr. Earring +1",ear2="Loquacious Earring",
 	body="Zendik Robe",hands=gear.herculean_dt_hands,ring1="Lebeche Ring",ring2="Prolix Ring",
 	back="Perimede Cape",waist="Isa Belt",legs="Rawhide Trousers",feet="Regal Pumps +1"}
-        
+	
 	sets.midcast.Dia = set_combine(sets.midcast.FastRecast, sets.TreasureHunter)
 	sets.midcast.Diaga = set_combine(sets.midcast.FastRecast, sets.TreasureHunter)
 	sets.midcast['Dia II'] = set_combine(sets.midcast.FastRecast, sets.TreasureHunter)
 	sets.midcast.Bio = set_combine(sets.midcast.FastRecast, sets.TreasureHunter)
 	sets.midcast['Bio II'] = set_combine(sets.midcast.FastRecast, sets.TreasureHunter)
-		
+	
     -- Midcast sets for pet actions
     sets.midcast.Pet.Cure = {}
 	sets.midcast.Pet['Enfeebling Magic'] = {neck="Adad Amulet",ear1="Enmerkar Earring",ear2="Handler's Earring +1",body="Taeon Tabard",hands="Regimen Mittens",ring1="Varar Ring +1",ring2="Varar Ring +1",waist="Incarnation Sash",legs="Tali'ah Sera. +2"}
     sets.midcast.Pet['Elemental Magic'] = {neck="Adad Amulet",ear1="Enmerkar Earring",ear2="Handler's Earring +1",body="Taeon Tabard",hands="Regimen Mittens",ring1="Varar Ring +1",ring2="Varar Ring +1",waist="Incarnation Sash",legs="Tali'ah Sera. +2"}
+	
+	-- The following sets are predictive and are equipped before we even know the ability will happen, as a workaround due to
+	-- the fact that start of ability packets are too late in the case of Pup abilities, WS, and certain spells.
+	sets.midcast.Pet.PetEnmityGear = {}
 	sets.midcast.Pet.PetWSGear = {neck="Shulmanu Collar",ear1="Enmerkar Earring",ear2="Handler's Earring +1",body="Taeon Tabard",hands="Regimen Mittens",ring1="Varar Ring +1",ring2="Varar Ring +1",back="Dispersal Mantle",waist="Incarnation Sash",legs="Tali'ah Sera. +2"}
 	
     sets.midcast.Pet.PetWSGear.Ranged = set_combine(sets.midcast.Pet.PetWSGear, {})
@@ -205,8 +209,7 @@ function init_gear_sets()
 	sets.midcast.Pet.PetWSGear.Nuke = set_combine(sets.midcast.Pet.PetWSGear, {})
     
 	-- Currently broken, preserved in case of future functionality.
-	sets.midcast.Pet.WeaponSkill = {}
-	
+	--sets.midcast.Pet.WeaponSkill = {}
 
     -- Sets to return to when not performing an action.
     
