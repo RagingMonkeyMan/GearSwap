65cf5007b34def98104777d4e157400f82e8dd44
KalesAndRancor <some___guy@hotmail.com>
2019-11-27 09:30:30 -0600

    Testing

--- libs/Sel-Mappings.lua
+++ libs/Sel-Mappings.lua
@@ -20,7 +20,8 @@ elements.quickdraw = {['Fire']='Fire', ['Ice']='Ice', ['Wind']='Wind', ['Earth']
 elements.enspell = {['Fire']='Fire', ['Ice']='Blizzard', ['Wind']='Aero', ['Earth']='Stone',
         ['Lightning']='Thunder', ['Water']='Water', ['Light']='Light', ['Dark']='Dark',}
 
-enspell_lookup = {['Enthunder']='Thunder', ['Enstone']='Earth', ['Enaero']='Wind', ['Enblizzard']='Ice',
+
+enspell_lookup = S{['Enthunder']='Thunder', ['Enstone']='Earth', ['Enaero']='Wind', ['Enblizzard']='Ice',
 		['Enfire']='Fire', ['Enwater']='Water', ['Enlight']='Light', ['Endark']='Dark',	
 		['Enthunder II']='Thunder', ['Enstone II']='Earth', ['Enaero II']='Wind', 
 		['Enblizzard II']='Ice', ['Enfire II']='Fire', ['Enwater II']='Water', ['Enlight II']='Light', 
