ded666d0b66ddc27e6dbcd54f663bb0bf0676048
MonkeyMan <59778265+RagingMonkeyMan@users.noreply.github.com>
2022-02-11 10:54:12 -0500

    Update to handle wardrobes 4-8

--- libs/Sel-Utility.lua
+++ libs/Sel-Utility.lua
@@ -2293,7 +2293,7 @@ function item_name_to_id(name)
 	if name == nil or name == 'empty' then
 		return 22299
 	else
-		return (player.inventory[name] or player.wardrobe[name] or player.wardrobe2[name] or player.wardrobe3[name] or player.wardrobe4[name] or {id=nil}).id
+		return (player.inventory[name] or player.wardrobe[name] or player.wardrobe2[name] or player.wardrobe3[name] or player.wardrobe4[name] or player.wardrobe5[name] or player.wardrobe6[name] or player.wardrobe7[name] or player.wardrobe8[name] or {id=nil}).id
 	end
 end
 
